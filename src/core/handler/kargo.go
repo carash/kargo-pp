59c22232cb06efea0b6fafec03f4573ced6a7572
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2019-07-18 13:13:45 +0700

    added handler stubs

---
+++ src/core/handler/kargo.go
@@ -0,0 +1,10 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type KargoHandler struct {
+}
+
+func (k *KargoHandler) HandleGetJob(c *gin.Context) {}
+
+func (k *KargoHandler) HandleGetBid(c *gin.Context) {}
