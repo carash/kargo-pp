1da49eee812d4be44884d05a9de5b3a92e7e0a1b
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2019-07-18 13:56:58 +0700

    added fields for job

--- src/core/class/job/job.go
+++ src/core/class/job/job.go
@@ -1,13 +1,41 @@
 package job
 
+import (
+	"time"
+
+	"github.com/carash/kargo-pp/src/core/class/user"
+)
+
 type Job struct {
+	ID          int
+	User        *user.User
+	Origin      *Location
+	Dest        *Location
+	Budget      int
+	Shipment    time.Time
+	Description string
 }
 
-func (b Job) Less(o Job) bool {
+func (b Job) LessOrigin(o Job) bool {
+	return true
+}
+
+func (b Job) LessDest(o Job) bool {
+	return true
+}
+
+func (b Job) LessBudget(o Job) bool {
+	return true
+}
+
+func (b Job) LessShipment(o Job) bool {
 	return true
 }
 
 type Location struct {
+	ID   int
+	Name string
+	Code string
 }
 
 func (v Location) Less(o Location) bool {
