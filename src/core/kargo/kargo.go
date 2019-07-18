a0e5af3d65997784d832e7e279aa8192f6c2caea
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2019-07-18 15:24:14 +0700

    implemented controller

--- src/core/kargo/kargo.go
+++ src/core/kargo/kargo.go
@@ -4,6 +4,7 @@ import (
 	"github.com/carash/kargo-pp/src/core/class/bid"
 	"github.com/carash/kargo-pp/src/core/class/job"
 	"github.com/carash/kargo-pp/src/core/kargosql"
+	"github.com/carash/kargo-pp/src/util/kargosort"
 )
 
 type Kargo struct {
@@ -12,16 +13,40 @@ type Kargo struct {
 
 type BidParam struct {
 	UserID int
+	SortBy string
 }
 
 func (k *Kargo) GetBid(param BidParam) ([]bid.Bid, error) {
-	return nil, nil
+	bids, _ := k.SQLConn.GetBid(param.UserID)
+
+	switch param.SortBy {
+	case "vehicle":
+		kargosort.Sort(bids, func(i, j int) bool { return bids[i].LessVehicle(bids[j]) })
+	case "price":
+		kargosort.Sort(bids, func(i, j int) bool { return bids[i].LessPrice(bids[j]) })
+	}
+
+	return bids, nil
 }
 
 type JobParam struct {
 	UserID int
+	SortBy string
 }
 
 func (k *Kargo) GetJob(param JobParam) ([]job.Job, error) {
-	return nil, nil
+	jobs, _ := k.SQLConn.GetJob(param.UserID)
+
+	switch param.SortBy {
+	case "origin":
+		kargosort.Sort(jobs, func(i, j int) bool { return jobs[i].LessOrigin(jobs[j]) })
+	case "destination":
+		kargosort.Sort(jobs, func(i, j int) bool { return jobs[i].LessDest(jobs[j]) })
+	case "budget":
+		kargosort.Sort(jobs, func(i, j int) bool { return jobs[i].LessBudget(jobs[j]) })
+	case "shipment":
+		kargosort.Sort(jobs, func(i, j int) bool { return jobs[i].LessShipment(jobs[j]) })
+	}
+
+	return jobs, nil
 }
