b88e25f707010568a0e386c11f85e57530f67dc9
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2019-07-18 14:21:06 +0700

    implemented router

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/carash/kargo-pp/src/core/handler"
+	"github.com/carash/kargo-pp/src/core/kargo"
+	"github.com/carash/kargo-pp/src/core/kargosql"
+)
 
 func main() {
 	r := gin.Default()
+
+	sqlconn := kargosql.Connect()
+	kargocont := &kargo.Kargo{sqlconn}
+	kargohndl := &handler.KargoHandler{kargocont}
+
+	r.GET("/job", kargohndl.HandleGetJob)
+	r.GET("/bid", kargohndl.HandleGetBid)
+
 	r.Run("1234")
 }
