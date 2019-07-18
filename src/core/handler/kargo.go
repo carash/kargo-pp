4056b271f25b4671a4ee6f0f04bb702157ae9d20
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2019-07-18 14:37:40 +0700

    implemented job handler

--- src/core/handler/kargo.go
+++ src/core/handler/kargo.go
@@ -1,6 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/carash/kargo-pp/src/core/class/job"
 	"github.com/carash/kargo-pp/src/core/kargo"
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +14,54 @@ type KargoHandler struct {
 	KargoController *kargo.Kargo
 }
 
-func (k *KargoHandler) HandleGetJob(c *gin.Context) {}
+type JobResponse struct {
+	ErrorCode int       `json:"err_code,omitempty"`
+	ErrorMsg  string    `json:"err_msg,omitempty"`
+	Jobs      []job.Job `json:"jobs,omitempty"`
+}
+
+func (k *KargoHandler) HandleGetJob(c *gin.Context) {
+	var statusCode int = http.StatusOK
+	var errcode int
+	var errmsg string
+	var jobs []job.Job
+	defer func() {
+		data := JobResponse{
+			ErrorCode: errcode,
+			ErrorMsg:  errmsg,
+			Jobs:      jobs,
+		}
+
+		s, _ := json.Marshal(data)
+		c.Data(statusCode, "application/json", s)
+	}()
+
+	uidString, ok := c.GetQuery("user_id")
+	if !ok {
+		statusCode = http.StatusBadRequest
+		errcode = 1
+		errmsg = "need user id"
+		return
+	}
+
+	uid, err := strconv.Atoi(uidString)
+	if err != nil {
+		statusCode = http.StatusBadRequest
+		errcode = 2
+		errmsg = "user id must be int"
+		return
+	}
+
+	j, err := k.KargoController.GetJob(kargo.JobParam{UserID: uid})
+	if err != nil {
+		statusCode = http.StatusInternalServerError
+		errcode = 3
+		errmsg = "error retrieving bids"
+		return
+	}
+
+	jobs = j
+	return
+}
 
 func (k *KargoHandler) HandleGetBid(c *gin.Context) {}
